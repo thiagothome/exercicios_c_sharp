95083d6e891e6393b3c0bb1a60e46d253f8c4505
Thiago Gomes <thiago.thomers@gmail.com>
2023-09-23 13:19:45 -0300

    Adicionado Exercício de Gerenciamento de estoque. Primeira parte.

---
+++ ExerciciosDicionario/ExercicioGerenciaEstoque/GerenciamentoDeEstoque.cs
@@ -0,0 +1,85 @@
+using System;
+
+class GerenciamentoDeEstoque
+{
+    
+    public void GerenciarEstoque()
+    {
+        Dictionary<string, int> itens = AdicionarItens();
+        Console.WriteLine("Obrigado por utilizar nossos serviços!");
+		foreach (var item in itens)
+		{
+			Console.WriteLine($"{item.Key}:{item.Value}");
+		}
+
+
+	}
+
+
+    public Dictionary<string, int> AdicionarItens()
+    {
+        Dictionary<string, int> itens = new Dictionary<string, int>();
+
+        
+        while (true)
+        {
+
+			Console.WriteLine("Adicione um produto a lista.");
+			string produto = Console.ReadLine();
+
+			Console.WriteLine("Adicione a quantidade do produto.");
+			int quantidade = int.Parse(Console.ReadLine());
+			
+            itens.Add(produto, quantidade);
+
+            Console.WriteLine("Deseja adicinaro mais itens ? (S/N) ");
+            string opcao = Console.ReadLine();
+            opcao = opcao.ToUpper();
+
+            if (opcao == "N")
+            {
+                return itens;
+            }else if (opcao == "S")
+            {
+				Console.WriteLine("Adicione um produto a lista.");
+				produto = Console.ReadLine();
+
+				Console.WriteLine("Adicione a quantidade do produto.");
+				quantidade = int.Parse(Console.ReadLine());
+
+				itens.Add(produto, quantidade);
+
+				Console.WriteLine("Deseja adicinaro mais itens ? (S/N) ");
+				opcao = Console.ReadLine();
+				opcao = opcao.ToUpper();
+
+				if (opcao == "N")
+                {
+                    return itens;
+				}
+
+			}
+			else if (opcao != "S" && opcao != "N")
+            {
+                while (opcao != "S" && opcao != "N")
+                {
+					Console.WriteLine("Opcão inválida.");
+					Console.WriteLine("Deseja adicinaro mais itens ? (S/N) ");
+					opcao = Console.ReadLine();
+					opcao = opcao.ToUpper();
+
+				}
+                return itens;
+            }
+
+
+
+		}
+		
+
+
+
+
+	}
+
+}
---
+++ ExerciciosDicionario/ExercicioGerenciaEstoque/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        GerenciamentoDeEstoque ex = new GerenciamentoDeEstoque();
+
+        ex.GerenciarEstoque();
+    }
+
+}
