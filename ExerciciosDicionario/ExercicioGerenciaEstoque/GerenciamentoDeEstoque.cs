db505cedba7b9baf9454f7e5a50b491a3cfb41c4
Thiago Gomes <thiago.thomers@gmail.com>
2023-09-23 16:55:37 -0300

    Adicionado a função vender e procurar

--- ExerciciosDicionario/ExercicioGerenciaEstoque/GerenciamentoDeEstoque.cs
+++ ExerciciosDicionario/ExercicioGerenciaEstoque/GerenciamentoDeEstoque.cs
@@ -2,59 +2,107 @@ using System;
 
 class GerenciamentoDeEstoque
 {
-    
+    Dictionary<string, int> itens = new Dictionary<string, int>();
+
     public void GerenciarEstoque()
     {
-        Dictionary<string, int> itens = AdicionarItens();
-        Console.WriteLine("Obrigado por utilizar nossos serviços!");
-		foreach (var item in itens)
-		{
-			Console.WriteLine($"{item.Key}:{item.Value}");
-		}
+		Console.WriteLine($"{new string('-', 10)} Sistema de gerenciamento de estoque {new string('-', 10)}\t\n");
+
+		Console.WriteLine("Ainda não há produtos cadastrados.\n");
+		
+        string opcao = Menu();
+
+        while (opcao != "0")
+        {
+            switch (opcao)
+            {
+                case "0":
+                    return;
+                    break;
+                case "1":
+                    AdicionarItens();
+                    opcao = Menu();
+                    break;
+                case "2":
+                    foreach (var item in itens)
+                    {
+                        Console.WriteLine($"\n{item}");
+                    }
+                    opcao = Menu();
+                    break;
+                case "3":
+                    Console.WriteLine("Qual produto deseja vender ?");
+                    string produtoVenda = Console.ReadLine();
+                    itens.Remove( produtoVenda );
+                    if (itens.TryGetValue(produtoVenda, out int valor))
+                    {
+                        itens.Remove(produtoVenda);
+                    }
+                    opcao = Menu();
+                    break;
+                case "4":
+                    Console.WriteLine("Digite o produto que está procurando:");
+                    string procura = Console.ReadLine();
+                    foreach (var item in itens)
+                    {
+                        if (itens.ContainsKey(procura))
+                        {
+                            Console.WriteLine("Produto encontrado");
+                            Console.WriteLine(procura);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Produto inexistente");
+                        }
+                    }
+                    opcao = Menu();
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    opcao = Menu();
+                    break;
+            }
+        }
+
+    }
 
 
-	}
-
-
-    public Dictionary<string, int> AdicionarItens()
+    public void AdicionarItens()
     {
-        Dictionary<string, int> itens = new Dictionary<string, int>();
 
-        
         while (true)
         {
 
-			Console.WriteLine("Adicione um produto a lista.");
-			string produto = Console.ReadLine();
+			string produto;
+			int quantidade;
 
-			Console.WriteLine("Adicione a quantidade do produto.");
-			int quantidade = int.Parse(Console.ReadLine());
-			
+			DefineProdutos(out produto, out quantidade);
+
             itens.Add(produto, quantidade);
 
-			string opcao = Opcao();
+            string opcao = Opcao();
 
 			if (opcao == "N")
             {
-                return itens;
+                return;
             }else if (opcao == "S")
             {
-				Console.WriteLine("Adicione um produto a lista.");
-				produto = Console.ReadLine();
+                DefineProdutos(out produto, out quantidade);
 
-				Console.WriteLine("Adicione a quantidade do produto.");
-				quantidade = int.Parse(Console.ReadLine());
+                itens.Add(produto, quantidade);
 
-				itens.Add(produto, quantidade);
-
-				opcao = Opcao();
+                opcao = Opcao();
 
 				if (opcao == "N")
                 {
-                    return itens;
+                    return;
 				}
+                else
+                {
+                    continue;
+                }
 
-			}
+            }
 			else if (opcao != "S" && opcao != "N")
             {
                 while (opcao != "S" && opcao != "N")
@@ -63,7 +111,7 @@ class GerenciamentoDeEstoque
 					opcao = Opcao();
 					if (opcao == "N")
 					{
-						return itens;
+						return;
 					}
 
 				}
@@ -79,5 +127,30 @@ class GerenciamentoDeEstoque
 
 		return opcao;
 	}
+
+	public void DefineProdutos(out string produtoP, out int quantidadeQ)
+	{
+        Console.WriteLine("Adicione um produto a lista.\t\n");
+        produtoP = Console.ReadLine();
+
+        Console.WriteLine("Adicione a quantidade do produto.");
+        quantidadeQ = int.Parse(Console.ReadLine());
+
+    }
+
+    public string Menu()
+    {
+        Console.WriteLine("\n"+@$"Escolha uma das opções:
+
+Cadastrar produtos: 1
+Listar produtos: 2
+Vender um produto: 3
+Encontrar produto: 4
+Sair: 0" + "\n");
+
+        string opcao = Console.ReadLine();
+        return opcao;
+    }
+
 
 }
